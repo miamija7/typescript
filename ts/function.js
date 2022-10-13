d2ab329b083b12bb02547c93237b8d5872cb0bb2
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-10-13 16:39:20 -0500

    functions

--- ts/function.js
+++ ts/function.js
@@ -1,72 +1,49 @@
 // ANNOTATING FUNCTION PARAMETERS
-
-function square(num: number){
+function square(num) {
     return num * num;
 }
-
-const doSomething = (person: string, age: number, isFunny: boolean)=>{};
-
+var doSomething = function (person, age, isFunny) { };
 square(3);
 doSomething("Chicken", 78, true);
-
-
-
 // ANNOTATING FUNCTION RETURN VALUES
-
-const addNums = (x: number, y:number): number => {
+var addNums = function (x, y) {
     return x + y;
+};
+function greet(person) {
+    if (person === void 0) { person = "stranger"; }
+    return "Hi there, ".concat(person, "!");
 }
-
-function greet(person: string = "stranger"): string {
-    return `Hi there, ${person}!`
-}
-
-addNums(5,5);
+addNums(5, 5);
 greet("Tonya Harding");
 greet();
-
-
-
 // ANONYMOUS FUNCTIONS
-
-const numbers = [1, 2, 3];
-numbers.forEach(num => {
+var numbers = [1, 2, 3];
+numbers.forEach(function (num) {
     // return num.toUpperCase();  // Error!
-})
-
-const colors = ["red", "orange", "yellow"];
-colors.map(color => {
+});
+var colors = ["red", "orange", "yellow"];
+colors.map(function (color) {
     // return color.toFixed();      //Error!
     return color.toUpperCase();
-})
-
-
-
+});
 // VOID: A function that shouldn't return anything
-
-const annoyUser = (num: number) : void => {
-    for (let i = 0; i < num; i++){
+var annoyUser = function (num) {
+    for (var i = 0; i < num; i++) {
         alert('HIIIIIIII!!');
     }
-}
-
-function printTwice(msg: string): void {
+};
+function printTwice(msg) {
     console.log(msg);
     console.log(msg);
 }
-
-
-
 // NEVER: A function that should never return (throws an exception OR is continuously runs)
-
 // doesn't finish running
-const neverStop = (): never => {
-    while(true){
+var neverStop = function () {
+    while (true) {
         console.log("I'm still going!");
     }
-}
-
+};
 // throws an exception
-const giveError = (msg: string): never => {
+var giveError = function (msg) {
     throw new Error(msg);
-}
+};
